b6b32a8d6ded0ff9efd7c2561655cbdd87bb186d
Franco Herrera <fherrera@uarg.unpa.edu.ar>
2026-07-16 10:03:24 -0300

    Initial commit

---
+++ src/ejercicio_seis/UsoEjercicioSeis.java
@@ -0,0 +1,7 @@
+package ejercicio_seis;
+
+public class UsoEjercicioSeis {
+    public static void main(String[] args) {
+        System.out.println("Probando el Ejercicio 6 (Arreglo)...");
+    }
+}
