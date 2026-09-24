7e109482217c3512810aef8db2329e6f6d4b226d
Franco Herrera <fherrera@uarg.unpa.edu.ar>
2026-09-24 12:03:42 +0000

    fix: arreglar estructura del repo

--- src/estructuras/PilaInt.java
+++ src/estructuras/PilaInt.java
@@ -3,32 +3,6 @@ package estructuras;
 
 public class PilaInt {
 
-    private final int maxpila = 30;
-    private int[] elementos;
-    private int cima;
-
-    public PilaInt() {
-        elementos = new int[maxpila];
-        cima = -1;
-    }
-
-    public boolean estaVacia() {
-        return (cima == -1);
-    }
-
-    public boolean estaLlena() {
-        return (cima == maxpila - 1);
-    }
-
-    public void meter(int e) {
-        cima = cima + 1;
-        elementos[cima] = e;
-    }
-
-    public int sacar() {
-        int aux = elementos[cima];
-        cima = cima - 1;
-        return aux;
-    }
+     // --- TU CÓDIGO AQUÍ ---
 
 }
--- src/estructuras/PilaString.java
+++ src/estructuras/PilaString.java
@@ -3,32 +3,5 @@ package estructuras;
 
 public class PilaString {
 
-    private final int maxpila = 10;
-    private String[] elementos;
-    private int cima;
-
-    public PilaString() {
-        elementos = new String[maxpila];
-        cima = -1;
-    }
-
-    public boolean estaVacia() {
-        return (cima == -1);
-    }
-
-    public boolean estaLlena() {
-        return (cima == maxpila - 1);
-    }
-
-    public void meter(String e) {
-        cima = cima + 1;
-        elementos[cima] = e;
-    }
-
-    public String sacar() {
-        String aux = elementos[cima];
-        cima = cima - 1;
-        return aux;
-    }
-
+   // --- TU CÓDIGO AQUÍ ---
 }
